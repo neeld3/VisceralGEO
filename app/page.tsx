6b0eac9e31515e1fbe86a9a45e193ba13a6a157b
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-14 18:56:36 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-start">
       <div className="mt-24">
         <h1 className="text-4xl font-bold text-center">Copy Copy That</h1>
-        <p className="mt-8 text-lg text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.
-          Vestibulum at sapien ac urna blandit lacinia. Curabitur posuere augue sed magna volutpat, 
-          id mollis ex gravida. Sed quis leo nec lacus ullamcorper dignissim. Ut vestibulum metus 
-          a tellus maximus, nec consequat neque pellentesque. Sed non odio eu mi consectetur congue. 
-          Vivamus a libero nec sem commodo ultrices. Duis eget leo dignissim, efficitur nunc in, 
-          laoreet libero. Proin sodales turpis at ex ullamcorper, ac mattis nisi tristique. 
-          Integer suscipit turpis vel urna ultrices, vitae fermentum turpis posuere.
+        <p className="mt-8 mx-4 text-lg text-center">
+          Is there anything that you are passionate about sharing with the world? Do you dream of sharing your expertise and
+          insights with others, but find yourself overwhelmed by the thought of creating something from scratch?
+          
+          Copy Copy That is a product designed to help you share your knowledge. 
+
+          The idea is that your knowledge on a niche topic will be taken and condensed into a drip-learning format
+          to create 10 easily digestible yet still informative lessons.
+          
         </p>
       </div>
     </main>
