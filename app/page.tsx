5f4383c2795da11488135eacda04e0ff4d393cc7
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-14 18:35:31 -0400

    Update page.tsx
    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start">
-      <div className="mt-24">
-        <h1 className="text-4xl font-bold text-center">Copy Copy That</h1>
-      </div>
+    <main className="flex min-h-screen items-start justify-center">
+      <h1 className="text-4xl font-bold">Copy Copy That</h1>
     </main>
   );
 }
