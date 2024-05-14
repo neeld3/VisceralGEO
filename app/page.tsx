410db7be6967527061e1d98ff074162db2442d9f
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-14 18:38:32 -0400

    Update page.tsx
    test text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,41 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-start justify-center">
       <h1 className="text-4xl font-bold">Copy Copy That</h1>
+      <p className="mt-4 text-lg text-center">How to write irresistible copy (just like Ben Franklin).
+You, my fellow human, are great at learning a new musical instrument.
+Take anyone off the street, give them piano lessons a few times a week where they learn Jingle Bells and Happy Birthday, and within a month they WILL know how to play at least a few songs.
+So why isn't learning how to write this easy?
+
+Well, it is!
+
+It's called copywork.
+
+How Benjamin Franklin Taught Himself to Write Well
+Copywork is a discipline used to teach people how to write. For centuries, copywork was how we taught children to write well.
+
+So, hows it work?
+
+Simple: you copy, using your own handwriting, other writers' work. In doing so, you learn the texture, pattern, and habits of great writing.
+
+You don't learn a new musical instrument by writing your own songs. You learn other people's songs. Then, after mastering and finding what you like, you develop your own voice using the patterns you noticed in copying others.
+
+This is exactly how copywork helps you become a better writer.
+
+In The Autobiography of Benjamin Franklin, Benjamin Franklin tells a story about his father criticizing his writing.
+
+"About this time I met with an odd volume of the Spectator," Franklin wrote, "I thought the writing excellent, and wished, if possible, to imitate it"
+
+How to be a great copywriter
+In this course, I'll send you 1 effective sales letter a day for 10 days.
+
+The challenge starts every Monday, and you'll receive the letters every morning Monday through Friday.
+
+I'll explain the background of the letter, why it works, and what you can learn from it.
+
+You'll then spend 25-45 minutes writing out the sales letter by hand.
+
+At the end of this, you'll know what makes a sales letter great and how to improve your own copywriting.
+      </p>
     </main>
   );
 }
