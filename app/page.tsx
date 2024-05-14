b0480745f0c9bd3ba3ee7474ebc39d1f79c33c6e
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-14 18:44:35 -0400

    Update page.tsx
    rwar

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,8 @@ export default function Home() {
         Copy Copy That
       </h1>
       <p className="mt-4 text-lg text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. <br />
-          Vestibulum at sapien ac urna blandit lacinia. Curabitur posuere augue sed magna volutpat, <br />
-          id mollis ex gravida. Sed quis leo nec lacus ullamcorper dignissim. Ut vestibulum metus <br />
-          a tellus maximus, nec consequat neque pellentesque. Sed non odio eu mi consectetur congue. <br />
-          Vivamus a libero nec sem commodo ultrices. Duis eget leo dignissim, efficitur nunc in, <br />
-          laoreet libero. Proin sodales turpis at ex ullamcorper, ac mattis nisi tristique. <br />
-          Integer suscipit turpis vel urna ultrices, vitae fermentum turpis posuere.
-        </p>
+        test  
+        
       </p>
     </main>
   );
