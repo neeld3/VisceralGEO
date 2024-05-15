eb84901dd963b39548f486c4ebfc92b29470e00e
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-15 16:27:19 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
         />
         <h1 className="text-4xl font-bold text-center">Copy Copy That</h1>
         <p className="mt-8 text-lg">
-          Teaching can be a very daunting task. Sometimes it might not even be worth the time and effort. From planning interactive and catered lessons to the interpersonal demands, it&apos;s a lot. <br />
-          So for those who aren&apos;t seasoned educators but still have passions to share, this is where we come in. All you have to do is condense everything you see fit into a series of 10 PDF lessons. Each of these will be educational and empowering.<br />
-          Consider the possibilities. Whether you&apos;re a chef, coder, or  yoga instructor, there&apos;s an audience waiting to learn what you have to teach.<br />
-          With us, you can effortlessly monetize your expertise without the extra headaches.<br />
-          No overhead costs. No long process. No BS!<br />
-          Set your price and let your lessons flow to your students. Each course is drip-fed at the perfect pace to optimize learning and retention.<br />
+          Teaching can be a very daunting task. Sometimes it might not even be worth the time and effort. From planning interactive and catered lessons to the interpersonal demands, it&apos;s a lot. <br /><br />
+          So for those who aren&apos;t seasoned educators but still have passions to share, this is where we come in. All you have to do is condense everything you see fit into a series of 10 PDF lessons. Each of these will be educational and empowering.<br /><br />
+          Consider the possibilities. Whether you&apos;re a chef, coder, or  yoga instructor, there&apos;s an audience waiting to learn what you have to teach.<br /><br />
+          With us, you can effortlessly monetize your expertise without the extra headaches.<br /><br />
+          No overhead costs. No long process. No BS!<br /><br />
+          Set your price and let your lessons flow to your students. Each course is drip-fed at the perfect pace to optimize learning and retention.<br /><br />
         </p>
       </div>
     </main>
