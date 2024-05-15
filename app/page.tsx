7d0b79693d1507436dd9b524c5b986ae99f2eacf
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-15 16:45:18 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           No overhead costs. No long process. No BS!<br /><br />
           Set your price and let your lessons flow to your students. Each course is drip-fed at the perfect pace to optimize learning and retention.<br />
         </p>
-       <div className="mt-12 mx-auto max-w-lg">
+       <div className="mt-24">
         <button className="bg-white border border-black text-black py-2 px-4 rounded-lg font-semibold hover:bg-gray-100 transition duration-300">
           Start &apos;Teaching&apos; Now!
         </button>
