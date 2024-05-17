a9b8e70424a275654beddec524de720cd637966d
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-05-17 16:50:11 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       
       <div className="mt-12 mx-auto max-w-lg">
         <Image
-          src="/copyben.JPG"
-          alt="CopyBen"
+          src="/paint.JPG"
+          alt="Paint"
           width={500}
           height={300}
           className="mt-8 mx-auto"
@@ -31,7 +31,8 @@ export default function Home() {
           Are you an expert in some niche topic? If yes, you&apos;ve had the thought &quot;What if I could teach this for money?&quot; Then you looked into it a bit and said &quot;It&apos;s not worth the time and effort.&quot; From planning interactive and catered lessons to the interpersonal demands, it&apos;s a lot. <br /> <br />
           Or you might be a teacher who heard AI is coming for their job.<br /><br />
           We both know that there is much more intricacy and nuance to your craft than a machine can replicate.  But what if you could use this technology to your advantage? Ensuring your unique human touch and expertise remain irreplaceable.<br /><br />
-          With CopyCopyThat you can share your expertise for what you&apos;re passionate about. Do it without the extra steps and still maintain that touch of personality. Condense your knowledge into a series of 10 lessons, and watch. The money starts to flow without another finger lifted.<br /><br />
+         
+          With DripLearn you can think of us as the canvas to your expertise. Each drip of paint representing the incremental process of passing knowledge. Share your passion, without the extra steps, and still maintain that touch of personality. Just condense your knowledge into a series of 10 lessons, and watch. The money starts to flow without another finger lifted.<br /><br />
           No more grappling with the complexities of other demanding alternatives. We&apos;ve got you covered. Whether you specialize in poetry or comedic writing, your expertise is in demand.  Craft your course, set your price, and leave the rest to us. Each course is drip-fed at the perfect pace to optimize learning and retention.<br /><br />
           Share your passions with the world, and watch the cash flow in. No hassle, no fuss, pure earning and satisfaction.
           Click the button below to begin your journey as an educator and money-maker!<br />
