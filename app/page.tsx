20b13e004a6f499957b6b94952b1bec15e36d5a3
neeld3 <neel@dahake.com>
2024-06-06 13:02:38 -0400

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Head from 'next/head';
+import MyMap from '../components/map';
+
 
 export default function Home() {
   return (
     <main>
-    
+      <MyMap />
     </main>
   );
 }
---
+++ components/map.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import Map, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+const MyMap: React.FC = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14,
+  });
+
+  return (
+    <Map
+      initialViewState={viewport}
+      style={{ width: '100%', height: '600px' }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      mapboxAccessToken={MAPBOX_TOKEN}
+    >
+      <Marker longitude={-122.4} latitude={37.8} color="red" />
+    </Map>
+  );
+};
+
+export default MyMap;
