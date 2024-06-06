a84de4f506c90438a7ff6f9097a34a14f8caef0c
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-06 15:25:02 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
-import Image from "next/image";
+'use client'
+import * as React from 'react';
 import Head from 'next/head';
-import MyMap from '../components/map';
+import Map, {Marker} from 'react-map-gl';
 
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = "pk.eyJ1IjoibmVlbGRhaGFrZSIsImEiOiJjbHgzaDN6bGwwYzhqMmlvbWlneXVubGphIn0.ZcbsX4IIvVOYjAuPYKZisA"; // Set your mapbox token here
 
 export default function Home() {
   return (
-    <main>
-      <MyMap />
-    </main>
+    <div>
+      <Map
+        initialViewState={{
+          latitude: 40.7831,
+          longitude: -73.9712,
+          zoom: 14
+        }}
+        style={{width: "100vw", height: "100vh"}}
+        mapStyle="mapbox://styles/neeldahake/clx3jup4k00i701qogxka2fvc"
+        mapboxAccessToken={MAPBOX_TOKEN}
+      >
+        <Marker longitude={40.7831} latitude={73.9712} color="red" />
+        <div style={{ position: 'absolute', bottom: '50px', left: '50%', transform: 'translateX(-50%)' }}>
+          <button style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: 'gray', color: 'white', fontWeight: 'bold', border: 'none', borderRadius: '4px' }}>
+            Upload CSV
+          </button>
+        </div>
+      </Map>
+
+    </div>
   );
 }
