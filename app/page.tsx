d687d3b754475b044923f69d3faa3e2e42c51c58
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-11 12:37:07 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,16 @@
-'use client'
+'use client';
 
 import * as React from 'react';
 import Map, { Marker, Source, Layer } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { Pool } from 'pg'; 
 
-// PostgreSQL configuration
-const pool = new Pool({
-  user: 'postgres',
-  host: 'vgeo-1.c271tjjdrfsu.us-east-1.rds.amazonaws.com',
-  database: 'postgres',
-  password: '8ktR9MI:-0c]',
-  port: 5432, 
-});
-
-const MAPBOX_TOKEN = "pk.eyJ1IjoibmVlbGRhaGFrZSIsImEiOiJjbHgzaDN6bGwwYzhqMmlvbWlneXVubGphIn0.ZcbsX4IIvVOYjAuPYKZisA"; 
-const GEOJSON_URL = '/censustestblock.json'; 
+const GEOJSON_URL = '/censustestblock.json';
+const MAPBOX_TOKEN="pk.eyJ1IjoibmVlbGRhaGFrZSIsImEiOiJjbHgzaDN6bGwwYzhqMmlvbWlneXVubGphIn0.ZcbsX4IIvVOYjAuPYKZisA";
 
 export default function Home() {
   const [geojsonData, setGeojsonData] = React.useState(null);
-  const [dataFromDB, setDataFromDB] = React.useState(''); 
+  const [dataFromDB, setDataFromDB] = React.useState('test');
 
-  // Fetch GeoJSON data
   React.useEffect(() => {
     fetch(GEOJSON_URL)
       .then(response => response.json())
@@ -29,20 +18,12 @@ export default function Home() {
       .catch(error => console.error('Error fetching GeoJSON data:', error));
   }, []);
 
-  // Fetch data from PostgreSQL when component mounts
   React.useEffect(() => {
-    const fetchDataFromDB = async () => {
-      try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table'); 
-        setDataFromDB(result.rows[0].text_column); 
-        client.release();
-      } catch (error) {
-        console.error('Error fetching data from PostgreSQL:', error);
-      }
-    };
-    fetchDataFromDB(); 
-  }, []); 
+    fetch('/api/data')
+      .then(response => response.json())
+      .then(data => setDataFromDB(data.data.text_column))
+      .catch(error => console.error('Error fetching data from API:', error));
+  }, []);
 
   return (
     <div>
@@ -53,7 +34,7 @@ export default function Home() {
             longitude: -73.9712,
             zoom: 14
           }}
-          style={{width: "100vw", height: "100vh"}}
+          style={{ width: "100vw", height: "100vh" }}
           mapStyle="mapbox://styles/neeldahake/clx3jup4k00i701qogxka2fvc"
           mapboxAccessToken={MAPBOX_TOKEN}
         >
@@ -80,7 +61,7 @@ export default function Home() {
           <Marker longitude={-73.9712} latitude={40.7831} color="red" />
           <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
             <button className="px-4 py-2 bg-gray-700 text-white font-bold rounded-md">
-              {dataFromDB || 'Loading...'} {/* Display fetched data or Loading text */}
+              {dataFromDB}
             </button>
           </div>
         </Map>
