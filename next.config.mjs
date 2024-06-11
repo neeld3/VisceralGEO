1a8cbd457a781ffebf4847e158ec62e506fd0de4
Neel Dahake <34844844+neeld3@users.noreply.github.com>
2024-06-11 12:39:26 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  
-    webpack: (config, { webpack }) => {
-      // Optional: Enables reading mapbox token from environment variable
-      config.plugins.push(new webpack.EnvironmentPlugin({MapboxAccessToken: ''}));
-      return config;
-    }
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.EnvironmentPlugin({MapboxAccessToken: ''}));
+    return config;
+  }
+};
+
+export default nextConfig;
